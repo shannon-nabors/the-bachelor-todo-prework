cda4321d149ea8adaacad60be85a6f87cbd7104c
shannon-nabors <shannon.nabors@gmail.com>
2019-01-02 13:04:01 -0500

    Done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,56 @@
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  h = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        h = h+1
+      end
+    end
+  end
+  h
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_strings = []
+  data[season].each do |contestant|
+    age_strings << contestant["age"]
+  end
+  age_floats = []
+  age_strings.each do |age|
+    age_floats << age.to_f
+  end
+  sum = 0
+  age_floats.each do |age|
+    sum = sum + age
+  end
+  (sum / age_floats.length).round
 end
